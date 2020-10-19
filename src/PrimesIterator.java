15ec49042675f36730e293b3f6120ee9652ac1a5
Borek2222 <boris.bocek@educanet.cz>
2020-10-19 11:10:26 +0200

    hotovo

--- src/PrimesIterator.java
+++ src/PrimesIterator.java
@@ -14,25 +14,28 @@ public class PrimesIterator implements Iterator<Integer> {
 
     @Override
     public Integer next() {
-       do {
-           i++;
-       } while (!isPrime(i));
-       return i;
+        do {
+            i++;
+        } while (!isPrime(i));
+        return i;
     }
 
 
     public static void main(String[] args) {
         for (int prime : new PrimesIterable()) {
+            if (prime > 100) break;
             System.out.println(prime);
         }
     }
 
-    private static boolean isPrime(int i) {
-        // int sqroot = (int) Math.sqrt(i);
+    private  boolean isPrime(int i) {
+        int sqroot = (int) Math.sqrt(i);
         for (int j : primesCache) {
+            if (j > sqroot) break;
             if (i % j == 0)
                 return false;
         }
         primesCache.add(i);
         return true;
+    }
 }
