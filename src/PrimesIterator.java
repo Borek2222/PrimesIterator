77c5811c00d717455a501ed118705a97b5d6a30b
Borek2222 <boris.bocek@educanet.cz>
2020-10-19 10:58:23 +0200

    idea ukazuje errory nvm

---
+++ src/PrimesIterator.java
@@ -0,0 +1,38 @@
+import java.util.Iterator;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PrimesIterator implements Iterator<Integer> {
+
+    private List<Integer> primesCache = new ArrayList<>();
+    private int i = 1;
+
+    @Override
+    public boolean hasNext() {
+        return true;
+    }
+
+    @Override
+    public Integer next() {
+       do {
+           i++;
+       } while (!isPrime(i));
+       return i;
+    }
+
+
+    public static void main(String[] args) {
+        for (int prime : new PrimesIterable()) {
+            System.out.println(prime);
+        }
+    }
+
+    private static boolean isPrime(int i) {
+        // int sqroot = (int) Math.sqrt(i);
+        for (int j : primesCache) {
+            if (i % j == 0)
+                return false;
+        }
+        primesCache.add(i);
+        return true;
+}
